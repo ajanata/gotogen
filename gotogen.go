e4a84d3913c20542b9d2bac555cba12dc785b4c4
Andy Janata <ajanata@gmail.com>
2022-11-27 23:00:25 -0800

    rename menu to status for clarity

--- gotogen.go
+++ gotogen.go
@@ -30,7 +30,7 @@ const (
 type Gotogen struct {
 	framerate  uint
 	frameTime  time.Duration
-	status     Blinker
+	blinker    Blinker
 	boopDist   uint8
 	aX, aY, aZ int32 // accelerometer
 
@@ -38,16 +38,16 @@ type Gotogen struct {
 	faceMirror  Display
 	faceState   faceState
 
-	menuDisplay              Display
-	menuMirror               Display
-	menuText                 *textbuf.Buffer // TODO interface
-	menuState                statusState
-	menuStateChange          time.Time
-	menuMirrorSkip           uint8
-	menuMirrorDownmixChannel colorChannel
-	menuMirrorDownmixCutoff  uint8
-	rootMenu                 Menu
-	activeMenu               Menuable
+	statusDisplay              Display
+	statusMirror               Display
+	statusText                 *textbuf.Buffer // TODO interface
+	statusState                statusState
+	statusStateChange          time.Time
+	statusMirrorSkip           uint8
+	statusMirrorDownmixChannel colorChannel
+	statusMirrorDownmixCutoff  uint8
+	rootMenu                   Menu
+	activeMenu                 Menuable
 
 	driver Driver
 
@@ -103,25 +103,25 @@ type Blinker interface {
 	High()
 }
 
-func New(framerate uint, menu Display, status Blinker, driver Driver) (*Gotogen, error) {
+func New(framerate uint, status Display, blinker Blinker, driver Driver) (*Gotogen, error) {
 	if framerate == 0 {
 		return nil, errors.New("must run at least one frame per second")
 	}
-	if menu == nil {
-		return nil, errors.New("must provide menu display")
+	if status == nil {
+		return nil, errors.New("must provide status display")
 	}
 	if driver == nil {
 		return nil, errors.New("must provide driver")
 	}
 
 	return &Gotogen{
-		framerate:   framerate,
-		frameTime:   time.Second / time.Duration(framerate),
-		menuDisplay: menu,
-		menuMirror:  mirror.New(menu),
-		status:      status,
-		driver:      driver,
-		start:       time.Now(),
+		framerate:     framerate,
+		frameTime:     time.Second / time.Duration(framerate),
+		statusDisplay: status,
+		statusMirror:  mirror.New(status),
+		blinker:       blinker,
+		driver:        driver,
+		start:         time.Now(),
 	}, nil
 }
 
@@ -134,29 +134,29 @@ func (g *Gotogen) Init() error {
 
 	var err error
 	// TODO font size configurable
-	g.menuText, err = textbuf.New(g.menuDisplay, textbuf.FontSize6x8)
+	g.statusText, err = textbuf.New(g.statusDisplay, textbuf.FontSize6x8)
 	if err != nil {
-		return errors.New("init menu: " + err.Error())
+		return errors.New("init status: " + err.Error())
 	}
-	g.menuText.AutoFlush = true
+	g.statusText.AutoFlush = true
 
-	w, h := g.menuText.Size()
-	if w < 15 || h < 4 {
-		return errors.New("unusably small menu display")
+	w, h := g.statusText.Size()
+	if w < 20 || h < 8 {
+		return errors.New("unusably small status display")
 	}
 
-	err = g.menuText.SetLineInverse(0, "GOTOGEN BOOTING")
+	err = g.statusText.SetLineInverse(0, "GOTOGEN BOOTING")
 	if err != nil {
 		return errors.New("boot msg: " + err.Error())
 	}
 	// we already validated it has at least 4 lines
-	_ = g.menuText.SetY(1)
+	_ = g.statusText.SetY(1)
 	// we already know it was possible to print text so don't bother checking every time
-	_ = g.menuText.Print("Initialize devices")
+	_ = g.statusText.Print("Initialize devices")
 
 	faceDisplay, err := g.driver.EarlyInit()
 	if err != nil {
-		_ = g.menuText.PrintlnInverse(err.Error())
+		_ = g.statusText.PrintlnInverse(err.Error())
 		return errors.New("early init: " + err.Error())
 	}
 	if faceDisplay == nil {
@@ -165,35 +165,35 @@ func (g *Gotogen) Init() error {
 
 	g.faceDisplay = faceDisplay
 	g.faceMirror = mirror.New(faceDisplay)
-	_ = g.menuText.Println(".")
+	_ = g.statusText.Println(".")
 
 	// now that we have the face panels set up, we can put a loading image on them while LateInit runs
 	err = g.busy()
 	if err != nil {
-		_ = g.menuText.PrintlnInverse("load busy: " + err.Error())
+		_ = g.statusText.PrintlnInverse("load busy: " + err.Error())
 		return errors.New("load busy: " + err.Error())
 	}
 
-	_ = g.menuText.Println("CPUs: " + strconv.Itoa(runtime.NumCPU()))
+	_ = g.statusText.Println("CPUs: " + strconv.Itoa(runtime.NumCPU()))
 	mem := runtime.MemStats{}
 	runtime.ReadMemStats(&mem)
 	g.totalRAM = strconv.Itoa(int(mem.HeapSys / 1024))
-	_ = g.menuText.Println(strconv.Itoa(int(mem.HeapSys/1024)) + "k RAM, " + strconv.Itoa(int(mem.HeapIdle/1024)) + "k free")
+	_ = g.statusText.Println(strconv.Itoa(int(mem.HeapSys/1024)) + "k RAM, " + strconv.Itoa(int(mem.HeapIdle/1024)) + "k free")
 
-	g.driver.LateInit(g.menuText)
+	g.driver.LateInit(g.statusText)
 	g.initMainMenu()
 
-	_ = g.menuText.Println("The time is now")
-	_ = g.menuText.Println(time.Now().Format(time.Stamp))
-	_ = g.menuText.Println("Booted in " + time.Now().Sub(g.start).Round(100*time.Millisecond).String())
-	_ = g.menuText.Println("Gotogen online.")
+	_ = g.statusText.Println("The time is now")
+	_ = g.statusText.Println(time.Now().Format(time.Stamp))
+	_ = g.statusText.Println("Booted in " + time.Now().Sub(g.start).Round(100*time.Millisecond).String())
+	_ = g.statusText.Println("Gotogen online.")
 
-	g.menuText.AutoFlush = false
-	g.menuStateChange = time.Now()
+	g.statusText.AutoFlush = false
+	g.statusStateChange = time.Now()
 
 	// TODO load from settings storage; this is also defined in initMainMenu
-	g.menuMirrorSkip = 4
-	g.menuMirrorDownmixCutoff = 0xA0
+	g.statusMirrorSkip = 4
+	g.statusMirrorDownmixCutoff = 0xA0
 
 	g.blink()
 	g.init = true
@@ -229,7 +229,7 @@ func (g *Gotogen) RunTick() error {
 		return errors.New("not initialized")
 	}
 
-	g.statusOff()
+	g.blinkerOff()
 	g.tick++
 
 	// busy states clear when we get back to the run loop
@@ -258,9 +258,9 @@ func (g *Gotogen) RunTick() error {
 	}
 
 	// TODO better way to framerate limit the status screen
-	canRedrawStatus := g.menuDisplay.CanUpdateNow()
-	if g.menuMirrorSkip > 0 {
-		canRedrawStatus = canRedrawStatus && uint8(g.tick)%g.menuMirrorSkip == 0
+	canRedrawStatus := g.statusDisplay.CanUpdateNow()
+	if g.statusMirrorSkip > 0 {
+		canRedrawStatus = canRedrawStatus && uint8(g.tick)%g.statusMirrorSkip == 0
 	}
 	// we always need to call this tho since the menu handling code is in here
 	g.updateStatus(canRedrawStatus)
@@ -282,14 +282,14 @@ func (g *Gotogen) RunTick() error {
 		g.panic(err)
 	}
 
-	if g.menuState != statusStateBlank && canRedrawStatus {
-		err = g.menuText.Display()
+	if g.statusState != statusStateBlank && canRedrawStatus {
+		err = g.statusText.Display()
 		if err != nil {
 			g.panic(err)
 		}
 	}
 
-	g.statusOn()
+	g.blinkerOn()
 	return nil
 }
 
@@ -297,17 +297,17 @@ func (g *Gotogen) drawIdleStatus() {
 	mem := runtime.MemStats{}
 	runtime.ReadMemStats(&mem)
 	// TODO switch which line this is on every minute or so for burn-in protection
-	_ = g.menuText.SetLine(0, time.Now().Format("03:04"), " ", strconv.Itoa(int(g.lastFPS)), "Hz ", strconv.Itoa(int(mem.HeapIdle/1024)), "k/", g.totalRAM, "k")
+	_ = g.statusText.SetLine(0, time.Now().Format("03:04"), " ", strconv.Itoa(int(g.lastFPS)), "Hz ", strconv.Itoa(int(mem.HeapIdle/1024)), "k/", g.totalRAM, "k")
 	// TODO temp hack
-	_ = g.menuText.SetLine(1, strconv.Itoa(int(g.boopDist)), " ", strconv.Itoa(int(g.aX)), " ", strconv.Itoa(int(g.aY)), " ", strconv.Itoa(int(g.aZ)))
+	_ = g.statusText.SetLine(1, strconv.Itoa(int(g.boopDist)), " ", strconv.Itoa(int(g.aX)), " ", strconv.Itoa(int(g.aY)), " ", strconv.Itoa(int(g.aZ)))
 
 	// println(time.Now().Format("03:04"), g.lastFPS, "Hz", mem.HeapIdle/1024, "k/", g.totalRAM)
 }
 
 func (g *Gotogen) updateStatus(updateIdleStatus bool) {
-	switch g.menuState {
+	switch g.statusState {
 	case statusStateBoot:
-		if time.Now().After(g.menuStateChange.Add(menuTimeout)) {
+		if time.Now().After(g.statusStateChange.Add(menuTimeout)) {
 			g.changeStatusState(statusStateIdle)
 			break
 		}
@@ -325,14 +325,14 @@ func (g *Gotogen) updateStatus(updateIdleStatus bool) {
 			g.drawIdleStatus()
 		}
 	case statusStateMenu:
-		if time.Now().After(g.menuStateChange.Add(menuTimeout)) {
+		if time.Now().After(g.statusStateChange.Add(menuTimeout)) {
 			g.changeStatusState(statusStateIdle)
 			break
 		}
 
 		switch g.driver.PressedButton() {
 		case MenuButtonBack:
-			g.menuStateChange = time.Now()
+			g.statusStateChange = time.Now()
 			if g.activeMenu.Prev() == nil {
 				// at top level menu
 				g.changeStatusState(statusStateIdle)
@@ -340,10 +340,10 @@ func (g *Gotogen) updateStatus(updateIdleStatus bool) {
 				m := g.activeMenu
 				g.activeMenu = g.activeMenu.Prev()
 				m.SetPrev(nil)
-				g.activeMenu.Render(g.menuText)
+				g.activeMenu.Render(g.statusText)
 			}
 		case MenuButtonMenu:
-			g.menuStateChange = time.Now()
+			g.statusStateChange = time.Now()
 			switch active := g.activeMenu.(type) {
 			case *Menu:
 				// in case a menu is empty for some reason
@@ -353,45 +353,45 @@ func (g *Gotogen) updateStatus(updateIdleStatus bool) {
 				switch item := active.Items[active.selected].(type) {
 				case *Menu:
 					item.prev, g.activeMenu = g.activeMenu, item
-					item.Render(g.menuText)
+					item.Render(g.statusText)
 				case *ActionItem:
 					item.Invoke()
 				case *SettingItem:
 					item.prev, g.activeMenu = g.activeMenu, item
 					item.selected = item.Active
-					_, h := g.menuText.Size()
+					_, h := g.statusText.Size()
 					if item.selected > item.top+uint8(h)-2 {
 						// TODO avoid empty lines at the bottom?
 						item.top = item.selected
 					}
-					item.Render(g.menuText)
+					item.Render(g.statusText)
 				}
 			case *SettingItem:
 				active.Active = active.selected
 				active.Apply(active.selected)
 				g.activeMenu, active.prev = active.prev, nil
-				g.activeMenu.Render(g.menuText)
+				g.activeMenu.Render(g.statusText)
 			}
 		case MenuButtonUp:
-			g.menuStateChange = time.Now()
+			g.statusStateChange = time.Now()
 			if g.activeMenu.Selected() > 0 {
 				g.activeMenu.SetSelected(g.activeMenu.Selected() - 1)
 			}
 			if g.activeMenu.Selected() < g.activeMenu.Top() {
 				g.activeMenu.SetTop(g.activeMenu.Selected())
 			}
-			g.activeMenu.Render(g.menuText)
+			g.activeMenu.Render(g.statusText)
 		case MenuButtonDown:
-			g.menuStateChange = time.Now()
+			g.statusStateChange = time.Now()
 			g.activeMenu.SetSelected(g.activeMenu.Selected() + 1)
 			if g.activeMenu.Selected() > g.activeMenu.Len()-1 {
 				g.activeMenu.SetSelected(g.activeMenu.Len() - 1)
 			}
-			_, h := g.menuText.Size()
+			_, h := g.statusText.Size()
 			if g.activeMenu.Selected() > g.activeMenu.Top()+uint8(h)-2 {
 				g.activeMenu.SetTop(g.activeMenu.Top() + 1)
 			}
-			g.activeMenu.Render(g.menuText)
+			g.activeMenu.Render(g.statusText)
 		}
 	case statusStateBlank:
 		if g.driver.PressedButton() != MenuButtonNone {
@@ -402,22 +402,22 @@ func (g *Gotogen) updateStatus(updateIdleStatus bool) {
 
 func (g *Gotogen) clearStatusScreen() {
 	// clear text buffer
-	g.menuText.Clear()
+	g.statusText.Clear()
 	// but make sure we clear the *entire* screen, including pixels outside the coverage of the text buffer
-	w, h := g.menuDisplay.Size()
+	w, h := g.statusDisplay.Size()
 	for x := int16(0); x < w; x++ {
 		for y := int16(0); y < h; y++ {
-			g.menuDisplay.SetPixel(x, y, color.RGBA{})
+			g.statusDisplay.SetPixel(x, y, color.RGBA{})
 		}
 	}
-	_ = g.menuDisplay.Display()
+	_ = g.statusDisplay.Display()
 }
 
 func (g *Gotogen) changeStatusState(state statusState) {
 	println("changing to status state", state.String())
 	g.activeMenu = nil
-	g.menuState = state
-	g.menuStateChange = time.Now()
+	g.statusState = state
+	g.statusStateChange = time.Now()
 	g.clearStatusScreen()
 
 	switch state {
@@ -430,7 +430,7 @@ func (g *Gotogen) changeStatusState(state statusState) {
 		m := g.rootMenu.Items[0].(*Menu)
 		m.Items = g.driver.MenuItems()
 		g.activeMenu = &g.rootMenu
-		g.rootMenu.Render(g.menuText)
+		g.rootMenu.Render(g.statusText)
 	}
 }
 
@@ -445,27 +445,26 @@ func (g *Gotogen) panic(v any) {
 }
 
 func (g *Gotogen) blink() {
-	g.statusOn()
+	g.blinkerOn()
 	time.Sleep(100 * time.Millisecond)
-	g.statusOff()
+	g.blinkerOff()
 	time.Sleep(100 * time.Millisecond)
 }
 
-func (g *Gotogen) statusOn() {
-	if g.status != nil {
-		g.status.High()
+func (g *Gotogen) blinkerOn() {
+	if g.blinker != nil {
+		g.blinker.High()
 	}
 }
 
-func (g *Gotogen) statusOff() {
-	if g.status != nil {
-		g.status.Low()
+func (g *Gotogen) blinkerOff() {
+	if g.blinker != nil {
+		g.blinker.Low()
 	}
 }
 
 func (g *Gotogen) SetFullFace(img image.Image, offX, offY int16, drawStatusMirror bool) {
-	drawStatusMirror = drawStatusMirror && g.menuDisplay.CanUpdateNow() && g.menuState == statusStateIdle
-	// drawStatusMirror = drawStatusMirror && g.menuState == statusStateIdle
+	drawStatusMirror = drawStatusMirror && g.statusDisplay.CanUpdateNow() && g.statusState == statusStateIdle
 
 	w, h := g.faceMirror.Size()
 	b := img.Bounds()
@@ -484,31 +483,31 @@ func (g *Gotogen) SetFullFace(img image.Image, offX, offY int16, drawStatusMirro
 
 func (g *Gotogen) downmixForStatus(x, y int16, h int16, r, gr, b uint8) {
 	// RGBA returns each channel |= itself << 8 for whatever reason
-	switch g.menuMirrorDownmixChannel {
+	switch g.statusMirrorDownmixChannel {
 	case red:
 		r &= 0xFF
-		if r < g.menuMirrorDownmixCutoff {
+		if r < g.statusMirrorDownmixCutoff {
 			r = 0
 		}
 		gr = 0
 		b = 0
 	case green:
 		gr &= 0xFF
-		if gr < g.menuMirrorDownmixCutoff {
+		if gr < g.statusMirrorDownmixCutoff {
 			gr = 0
 		}
 		r = 0
 		b = 0
 	case blue:
 		b &= 0xFF
-		if b < g.menuMirrorDownmixCutoff {
+		if b < g.statusMirrorDownmixCutoff {
 			b = 0
 		}
 		r = 0
 		gr = 0
 	}
 	// FIXME remove hardcoded offset
-	g.menuMirror.SetPixel(x, 32+y%h, color.RGBA{R: r, G: gr, B: b, A: 1})
+	g.statusMirror.SetPixel(x, 32+y%h, color.RGBA{R: r, G: gr, B: b, A: 1})
 }
 
 func (g *Gotogen) initMainMenu() {
@@ -527,51 +526,50 @@ func (g *Gotogen) initMainMenu() {
 				Name:    "Status mirror skip",
 				Options: []string{"0", "1", "2", "4", "8", "16"},
 				Active:  3, // TODO load from setting storage
-				Apply:   g.setMenuMirrorSkip,
+				Apply:   g.setStatusMirrorSkip,
 			},
 			&SettingItem{
 				Name:    "Status mirror color",
 				Options: []string{"red", "green", "blue"},
 				Active:  0,
-				Apply:   g.setMenuMirrorColor,
+				Apply:   g.setStatusMirrorColor,
 			},
 			&SettingItem{
 				Name:    "Status mirror cutoff",
 				Options: []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "D", "E", "F"},
 				Active:  9,
-				Apply:   g.setMenuMirrorCutoff,
+				Apply:   g.setStatusMirrorCutoff,
 			},
 		},
 	}
 }
 
-func (g *Gotogen) setMenuMirrorCutoff(selected uint8) {
-	g.menuMirrorDownmixCutoff = (selected + 1) << 4
+func (g *Gotogen) setStatusMirrorCutoff(selected uint8) {
+	g.statusMirrorDownmixCutoff = (selected + 1) << 4
 }
 
-func (g *Gotogen) setMenuMirrorColor(selected uint8) {
-	g.menuMirrorDownmixChannel = colorChannel(selected)
+func (g *Gotogen) setStatusMirrorColor(selected uint8) {
+	g.statusMirrorDownmixChannel = colorChannel(selected)
 }
 
-func (g *Gotogen) setMenuMirrorSkip(selected uint8) {
+func (g *Gotogen) setStatusMirrorSkip(selected uint8) {
 	if selected == 0 {
-		g.menuMirrorSkip = 0
+		g.statusMirrorSkip = 0
 	} else {
-		g.menuMirrorSkip = 1 << (selected - 1)
+		g.statusMirrorSkip = 1 << (selected - 1)
 	}
-	println("setting menu mirror skip to", g.menuMirrorSkip)
 }
 
 func (g *Gotogen) Busy(f func(buffer *textbuf.Buffer)) {
-	g.menuText.AutoFlush = true
-	g.menuText.Clear()
+	g.statusText.AutoFlush = true
+	g.statusText.Clear()
 
 	err := g.busy()
 	if err != nil {
 		print("unable to load busy", err)
-		_ = g.menuText.PrintlnInverse("loading busy: " + err.Error())
+		_ = g.statusText.PrintlnInverse("loading busy: " + err.Error())
 	}
-	f(g.menuText)
+	f(g.statusText)
 
 	s := time.Now()
 	for time.Now().Before(s.Add(5 * time.Second)) {
@@ -580,7 +578,7 @@ func (g *Gotogen) Busy(f func(buffer *textbuf.Buffer)) {
 		}
 	}
 
-	g.menuText.AutoFlush = false
+	g.statusText.AutoFlush = false
 	g.changeStatusState(statusStateIdle)
 }
 
